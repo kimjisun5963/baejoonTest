3f9ad25ffb89af50d6df63662fd50fc1939a3fb0
kimjisun5963 <127927146+kimjisun5963@users.noreply.github.com>
2024-08-12 09:57:34 +0900

    [Bronze V] Title: 나머지, Time: 168 ms, Memory: 17716 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.java
@@ -0,0 +1,20 @@
+import java.util.*;
+//(A+B)%C는 ((A%C) + (B%C))%C 와 같을까?
+//(A×B)%C는 ((A%C) × (B%C))%C 와 같을까?
+//세 수 A, B, C가 주어졌을 때, 위의 네 가지 값을 구하는 프로그램을 작성하시오.
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int C = sc.nextInt();
+        int result1 = (A+B)%C;
+        int result2 = ((A%C) + (B%C))%C;
+        int result3 = (A*B)%C;
+        int result4 = ((A%C) * (B%C))%C;
+        System.out.println(result1);
+        System.out.println(result2);
+        System.out.println(result3);
+        System.out.println(result4);
+    }
+}
