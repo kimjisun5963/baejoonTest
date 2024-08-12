c23ebaeb36f0b051fefd7dd6ddd997bbd2a5e357
kimjisun5963 <127927146+kimjisun5963@users.noreply.github.com>
2024-08-12 10:47:49 +0900

    [Bronze V] Title: 고양이, Time: 116 ms, Memory: 13972 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,12 @@
+public class Main{
+//\    /\
+// )  ( ')
+//(  /  )
+// \(__)|
+    public static void main(String[] args){
+        System.out.println("\\    /\\");
+        System.out.println(" )"+"  ( \')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");   
+    }
+}
